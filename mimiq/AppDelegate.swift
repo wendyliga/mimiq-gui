2e1ef2489d7a52241f9d297b131701d22172dc9d
Wendy Liga <agilydnew@gmail.com>
2020-04-12 00:58:44 +0700

    initial commit

---
+++ mimiq/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mimiq
+//
+//  Created by Wendy Liga on 12/04/20.
+//  Copyright © 2020 Wendy Liga. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
