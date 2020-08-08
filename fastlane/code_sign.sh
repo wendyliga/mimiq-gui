af569d9f236a89b32cab2e09aa5271f4ca195589
Wendy Liga <agilydnew@gmail.com>
2020-08-08 15:10:30 +0700

    revert changes

--- fastlane/code_sign.sh
+++ fastlane/code_sign.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# codesign --verbose --force --deep -o runtime \
-#     --sign "Developer ID Application" \
-#     "../framework/Sparkle.framework/Versions/A/Resources/AutoUpdate.app"
-# codesign --verbose --force -o runtime \
-#     --sign "Developer ID Application" \
-#     "../mimiq/ffmpeg"
-# codesign --verbose --force -o runtime \
-#     --sign "Developer ID Application" \
-#     "../mimiq/mimiq"
+codesign --verbose --force --deep -o runtime \
+    --sign "Developer ID Application" \
+    "../framework/Sparkle.framework/Versions/A/Resources/AutoUpdate.app"
+codesign --verbose --force -o runtime \
+    --sign "Developer ID Application" \
+    "../mimiq/ffmpeg"
+codesign --verbose --force -o runtime \
+    --sign "Developer ID Application" \
+    "../mimiq/mimiq"
